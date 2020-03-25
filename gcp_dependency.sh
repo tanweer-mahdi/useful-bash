e8a5db8a8bb329eba82cdf4183d7a2ce99217f85
Shah Mahdi Hasan <shahmahdi.hasan@uon.edu.au>
2020-03-25 18:03:41 +1100

    Add files via upload

--- gcp_dependency.sh
+++ gcp_dependency.sh
@@ -2,13 +2,16 @@
 sudo apt update
 sudo apt install software-properties-common
 sudo add-apt-repository ppa:deadsnakes/ppa
+sudo apt update
 sudo apt install python3.7
 python3.7 --version
 # installing the pip
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 python3.7 get-pip.py
+sudo apt install python-pip
+sudo apt install python3-pip
 # installing libgomp
 sudo apt-get install libgomp1
 # installing libraries
-pip install pandas
-pip3 install numpy
+#pip install pandas
+#pip3 install numpy
---
+++ package_install.sh
@@ -0,0 +1,20 @@
+#! /bin/sh
+python3.7 -m pip install pandas
+python3.7 -m pip3 install pandas
+python3.7 -m pip install numpy
+python3.7 -m pip3 install numpy
+python3.7 -m pip install matplotlib
+python3.7 -m pip3 install matplotlib
+python3.7 -m pip install seaborn
+python3.7 -m pip3 install seaborn
+python3.7 -m pip install datetime
+python3.7 -m pip3 install datetime
+python3.7 -m pip install tqdm
+python3.7 -m pip3 install tqdm
+python3.7 -m pip install calendar
+python3.7 -m pip3 install calendar
+python3.7 -m pip install sklearn
+python3.7 -m pip3 install sklearn
+python3.7 -m pip install xgboost
+python3.7 -m pip3 install xgboost
+
